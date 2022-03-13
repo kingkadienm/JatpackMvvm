8b1b3621fe05387c64ed623da4a55362e177ec49
wangzhengshuai <kingkadienm@163.com>
2022-03-13 22:23:10 +0800

    [feature] 提交遗漏

---
+++ app_core/core/src/main/java/com/wangzs/core/network/interceptor/RxCacheInterceptor.java
@@ -0,0 +1,69 @@
+package com.wangzs.core.network.interceptor;
+
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * @Description:
+ * @Author: wangzs
+ * @Date: 2022-03-10
+ * @Version:
+ */
+
+public class RxCacheInterceptor {
+
+    /**
+     * Rx缓存配置
+     *
+     * @return
+     */
+//    public static Cache create() {
+//        return new Cache(BaseApplication.getContext().getCacheDir(), 10 * 1024 * 1024); // 缓存大小10M
+//    }
+
+    /**
+     * 有网时的缓存设置
+     *
+     * @return
+     */
+    public static Interceptor onNetwork() {
+        return chain -> {
+            Request request = chain.request();
+            Response originalResponse = chain.proceed(request);
+            int maxAge = 30; // 在线缓存,单位:秒
+            return originalResponse.newBuilder()
+                    .removeHeader("Pragma") // 清除头信息，因为服务器如果不支持，会返回一些干扰信息，不清除下面无法生效
+                    .removeHeader("Cache-Control")
+                    .header("Cache-Control", "public, max-age=" + maxAge)
+                    .build();
+        };
+    }
+
+    /**
+     * 无网时的缓存设置
+     *
+     * @return
+     */
+    public static Interceptor notNetwork() {
+        return chain -> {
+            Request request = chain.request();
+//            if (!NetworkUtil.isConnected(BaseApplication.getContext())) {
+//                int maxStale = 12 * 60 * 60; // 缓存时间
+//                CacheControl tempCacheControl = new CacheControl.Builder()
+//                        .onlyIfCached()
+//                        .maxStale(maxStale, TimeUnit.SECONDS)
+//                        .build();
+//                request = request.newBuilder()
+//                        .cacheControl(tempCacheControl)
+//                        .build();
+//            }
+            return chain.proceed(request);
+        };
+    }
+}
---
+++ app_core/core/src/main/java/com/wangzs/core/network/interceptor/RxEncryptInterceptor.java
@@ -0,0 +1,95 @@
+package com.wangzs.core.network.interceptor;
+
+
+import com.wangzs.base.toolskit.LogUtil;
+import com.wangzs.core.network.RxUrl;
+import com.wangzs.core.network.bean.RxCommonParam;
+
+import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * @Description: network interceptor
+ * @Author: wangzs
+ * @Date: 2022-03-10
+ * @Version:
+ */
+public class RxEncryptInterceptor {
+
+    private static final String TAG = RxEncryptInterceptor.class.getName();
+
+    public static Interceptor create() {
+        return chain -> {
+            Request request = chain.request();
+            request = replaceHost(request);
+            LogUtil.d(TAG, "Network Request URL:" + request.url());
+            request = addCommonParams(request);
+            request = addHeader(request);
+            Response response = chain.proceed(request);
+            return response;
+        };
+    }
+
+    /**
+     * 替换 url
+     *
+     * @param request
+     * @return
+     */
+    private static Request replaceHost(Request request) {
+        Request.Builder builder = request.newBuilder();
+        HttpUrl newBaseUrl = HttpUrl.parse(RxUrl.getBaseUrl());
+        HttpUrl oldHttpUrl = request.url();
+        //重建新的HttpUrl，修改需要修改的url部分
+        HttpUrl newFullUrl = oldHttpUrl
+                .newBuilder()
+                .scheme(newBaseUrl.scheme())
+                .host(newBaseUrl.host())
+                .port(newBaseUrl.port())
+                .build();
+        return builder.url(newFullUrl).build();
+    }
+
+    private static Request addCommonParams(Request oldRequest) {
+        HttpUrl.Builder builder = oldRequest.url()
+                .newBuilder();
+        Map<String, Object> params = RxCommonParam.getCommonParams();
+        for (String key : params.keySet()) {
+            builder.setEncodedQueryParameter(key, String.valueOf(params.get(key)));
+        }
+        Request newRequest = oldRequest.newBuilder()
+                .method(oldRequest.method(), oldRequest.body())
+                .url(builder.build())
+                .build();
+        return newRequest;
+    }
+
+    /**
+     * Request数据加密
+     *
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    private static Request addHeader(Request request) {
+        Request.Builder requestBuilder = request.newBuilder();
+        HttpUrl url = request.url();
+        HttpUrl.Builder builder = url.newBuilder();
+        requestBuilder.url(builder.build())
+                .method(request.method(), request.body())
+                .addHeader("Content_Type", "application/json")
+                .addHeader("charset", "UTF-8");
+
+//        String cookie = RouterHelper.getService().getAccountService().getAccountCookie();
+//        if (!TextUtils.isEmpty(cookie)) {
+//            requestBuilder.addHeader("Cookie", cookie); // "JSESSIONID=constant-session-0"
+//        }
+        requestBuilder.addHeader("appVersion", "1.0.0");
+        return requestBuilder.build();
+    }
+}
